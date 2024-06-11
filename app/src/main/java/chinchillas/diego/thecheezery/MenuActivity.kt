8c04acaf9019b6551ee18c9504aa99483eb6275a
Chinchillas <79560217+chinchillas03@users.noreply.github.com>
2024-06-11 00:53:24 -0700

    Practica 5

--- app/src/main/java/chinchillas/diego/thecheezery/MenuActivity.kt
+++ app/src/main/java/chinchillas/diego/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package chinchillas.diego.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,32 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnColdDrinks : Button = findViewById(R.id.button_cold_drinks)
+        var btnHotDrinks : Button = findViewById(R.id.button_hot_drinks)
+        var btnSweet : Button = findViewById(R.id.button_sweets)
+        var btnSalties : Button = findViewById(R.id.button_salties)
+
+
+        btnColdDrinks.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnHotDrinks.setOnClickListener{
+            var intent: Intent = Intent(this, HotProductsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSweet.setOnClickListener{
+            var intent: Intent = Intent(this, SweetProducts::class.java)
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener{
+            var intent: Intent = Intent(this, SaltiesProducts::class.java)
+            startActivity(intent)
+        }
+
     }
 }
